041ca3dc76240bbf77067c9591eee7e5cdc39695
893839477@qq.com <ly625625>
2018-01-05 11:19:17 +0800

    修改代码

--- app/src/main/java/com/food/market/activity/BaseActivity.java
+++ app/src/main/java/com/food/market/activity/BaseActivity.java
@@ -1,10 +1,17 @@
 package com.food.market.activity;
 
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * Created by Kelly Li on 2017-07-02.
  */
 
 public class BaseActivity  extends FragmentActivity{
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
 }
